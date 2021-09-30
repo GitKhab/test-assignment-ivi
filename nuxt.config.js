9f1d68e98e339d4f243a1c80cea5281e2a1754d2
Vasil Khabibullin <gitkhab@yandex.ru>
2021-10-01 02:44:50 +0300

    Replace global meta title with titleTemplate

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'test-assignment-ivi',
+    titleTemplate: '%s — Тестовое задание для ivi',
     htmlAttrs: {
       lang: 'ru'
     },
