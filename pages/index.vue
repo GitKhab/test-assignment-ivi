4fb1c7003ec9d80c30efdef27e157b37a84c314f
Vasil Khabibullin <gitkhab@yandex.ru>
2021-10-01 03:38:14 +0300

    Add homepage meta

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
 <template lang="pug">
   div.home content
 </template>
+
+<script>
+  export default {
+    head: {
+      title: 'Главная',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Устройте кинотеатр у себя дома! Нажмите на рубин и... порекомендуйте меня в it-команду ivi.ru'
+        }
+      ]
+    }
+  }
+</script>
